e497522e5a323d04ebb74aa372caf9972d24b5ee
Igor Walkoff ( iVolume) <i.volume@yandex.ru>
2017-03-09 16:28:37 +0300

    simple html

---
+++ dist/countbuttons.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Created by iVolume on 09.03.2017.
+ */
+
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    (function () {
+        var qty = document.querySelector('.qty');
+    })();
+});
---
+++ src/countbuttons.js
@@ -0,0 +1,10 @@
+/**
+ * Created by iVolume on 09.03.2017.
+ */
+
+document.addEventListener('DOMContentLoaded', event => {
+
+    (function () {
+        let qty = document.querySelector('.qty');
+    })();
+});
