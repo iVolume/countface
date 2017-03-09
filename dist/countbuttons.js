7ae21718a67fc0d6eae35852cac38383cf7f9cc6
Igor Walkoff ( iVolume) <i.volume@yandex.ru>
2017-03-09 22:22:17 +0300

    ver. 0.0.2 - js&css added

--- dist/countbuttons.js
+++ dist/countbuttons.js
@@ -4,9 +4,49 @@
  * Created by iVolume on 09.03.2017.
  */
 
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener('DOMContentLoaded', function () {
 
     (function () {
-        var qty = document.querySelector('.qty');
+        var qty = document.querySelectorAll('.qty');
+        var decreaseEl = document.querySelectorAll('.decreaseBtn'),
+            increaseEl = document.querySelectorAll('.increaseBtn');
+
+        qty.forEach(function (current) {
+            return current.value == '' ? current.value = 1 : current.value;
+        });
+
+        decreaseEl.forEach(function (current) {
+            return current.addEventListener('click', decrease, false);
+        });
+        increaseEl.forEach(function (current) {
+            return current.addEventListener('click', increase, false);
+        });
+        qty.forEach(function (current) {
+            return current.addEventListener('keypress', qtyChange, false);
+        });
+
+        function decrease() {
+            var qty = this.parentElement.querySelector('.qty');
+
+            qty.value > 1 ? qty.value-- : qty.value = 1;
+        }
+
+        function increase() {
+            var qty = this.parentElement.querySelector('.qty');
+
+            qty.value++;
+        }
+
+        function qtyChange(event) {
+            if (event.keyCode == 13) {
+
+                var val = this.value;
+
+                if (val == '' || isNaN(val) || val < 1) {
+                    this.value = 1;
+                    this.blur();
+                }
+            }
+        }
     })();
 });
--- src/countbuttons.js
+++ src/countbuttons.js
@@ -2,9 +2,42 @@
  * Created by iVolume on 09.03.2017.
  */
 
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    (function () {
-        let qty = document.querySelector('.qty');
+    (() => {
+        let qty = document.querySelectorAll('.qty');
+        let decreaseEl = document.querySelectorAll('.decreaseBtn'),
+            increaseEl = document.querySelectorAll('.increaseBtn');
+
+        qty.forEach( (current) => current.value == '' ? current.value = 1 : current.value);
+
+        decreaseEl.forEach( (current) => current.addEventListener('click', decrease, false));
+        increaseEl.forEach( (current) => current.addEventListener('click', increase, false));
+        qty.forEach( (current) => current.addEventListener('keypress', qtyChange, false));
+
+        function decrease(){
+            let qty = this.parentElement.querySelector('.qty');
+
+            qty.value > 1 ? qty.value-- : qty.value = 1;
+        }
+
+        function increase() {
+            let qty = this.parentElement.querySelector('.qty');
+
+            qty.value++;
+        }
+
+        function qtyChange(event) {
+            if( event.keyCode == 13 ){
+
+                let val = this.value;
+
+                if(val == '' || isNaN(val) || val < 1){
+                    this.value = 1;
+                    this.blur();
+                }
+            }
+        }
+
     })();
 });
